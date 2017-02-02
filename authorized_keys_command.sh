860356412b2757bdb26ee51a47734686c76fda9a
shine <hello@shinenelson.com>
2017-02-02 14:33:08 +0530

    DEBUG logging on authorized_keys_command.sh

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
+echo "Invoked AuthorizedKeysCommand"
+
 if [ -z "$1" ]; then
+  echo "No User Specified"
   exit 1
 fi
+
+echo "Username : $1"
 
 SaveUserName="$1"
 SaveUserName=${SaveUserName//"+"/".plus."}
@@ -10,6 +15,9 @@ SaveUserName=${SaveUserName//"="/".equal."}
 SaveUserName=${SaveUserName//","/".comma."}
 SaveUserName=${SaveUserName//"@"/".at."}
 
+echo "SaveUserName : $SaveUserName"
+
 aws iam list-ssh-public-keys --user-name "$SaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
+  echo "KeyId : $KeyId"
   aws iam get-ssh-public-key --user-name "$SaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
 done
