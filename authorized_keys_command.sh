c79a50598ef9772c04ed143b7dfbb658d7416d03
shine <hello@shinenelson.com>
2017-02-05 03:02:26 +0530

    Revert "DEBUG logging on authorized_keys_command.sh"
    This reverts commit 860356412b2757bdb26ee51a47734686c76fda9a.

--- authorized_keys_command.sh
+++ authorized_keys_command.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-echo "Invoked AuthorizedKeysCommand"
-
 if [ -z "$1" ]; then
-  echo "No User Specified"
   exit 1
 fi
-
-echo "Username : $1"
 
 SaveUserName="$1"
 SaveUserName=${SaveUserName//"+"/".plus."}
@@ -15,9 +10,6 @@ SaveUserName=${SaveUserName//"="/".equal."}
 SaveUserName=${SaveUserName//","/".comma."}
 SaveUserName=${SaveUserName//"@"/".at."}
 
-echo "SaveUserName : $SaveUserName"
-
 aws iam list-ssh-public-keys --user-name "$SaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read KeyId; do
-  echo "KeyId : $KeyId"
   aws iam get-ssh-public-key --user-name "$SaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
 done
